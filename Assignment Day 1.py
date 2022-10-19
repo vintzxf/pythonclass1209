a101d650550fa10c94370137920f1eee4622bc1c
vintzxf <105851011+vintzxf@users.noreply.github.com>
2022-10-19 10:59:10 +0100

    Add files via upload

---
+++ Assignment Day 1.py
@@ -0,0 +1,20 @@
+#1
+print("MY NAME IS JAMES")
+
+
+#2
+state = "i'm from a well-known village with good culture in kogi state"
+print(state)
+
+
+#3
+TotalMoney = 1000
+Quantity = 3
+Price = 450
+expected_output =  ('i have {} so i can buy {} football for {} dollars')
+print(f'i have {TotalMoney}, so i can buy {Quantity} football for {Price} dollars')
+
+
+#4
+poem = 'hello world\nmy name is segun\ni love to code'
+print(poem)
---
+++ Day1.py
@@ -0,0 +1,30 @@
+word = ("python is fun\npython is easy\npython is interactive in nature")
+print(word)
+print("hello world")
+print("welcome to python class segun")
+
+
+
+price1 = 15000
+price2 = 30500
+price3 = 45000
+sales_report = 'i sold two pairs of shirts for {} and a suit for {}'
+print(sales_report.format(price1,price2))
+print(f'i sold two pairs of shirts for {price1}, two pairs of shirt for {price2} and a suit for {price3}')
+
+
+
+
+word1 = 'python'
+word2 = 'PYTHON'
+word3 = 'python is fun'
+word4 = '   info'
+print(word1.upper())
+print(word2.lower())
+print(word3.title())
+print(word3.capitalize())
+print(word3.split())
+print(word4.strip())
+
+
+
